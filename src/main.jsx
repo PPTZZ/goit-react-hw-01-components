bc6ea045c3a9f7088f238cc0e4e3375a029d743a
Alex Poputa <poputa.alex@gmail.com>
2024-10-07 15:05:33 +0300

    fixing gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TransactionHistoryPag from './pages/TransactionHistoryPag';
 import StatisticsPag from './pages/StatisticsPag';
 import ProfilePag from './pages/ProfilePag';
 import FriendsListPag from './pages/FriendsListPag';
-import App from './pages/App';
+import App from './pages/App.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<Routes>
 				<Route index path='/' element={<App />} />
-				<Route path='/profile' element={<ProfilePag />} />
-				<Route path='/friends' element={<FriendsListPag />} />
-				<Route path='/statistics' element={<StatisticsPag />} />
-				<Route path='/transactions' element={<TransactionHistoryPag />} />
+				<Route path='profile' element={<ProfilePag />} />
+				<Route path='friends' element={<FriendsListPag />} />
+				<Route path='statistics' element={<StatisticsPag />} />
+				<Route path='transactions' element={<TransactionHistoryPag />} />
 			</Routes>
-		</HashRouter>
+		</BrowserRouter>
 	</StrictMode>
 );
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,3 @@
-
 import { Link } from "react-router-dom";
 import style from '../css/pages/App.module.css'
 const App = () => {
