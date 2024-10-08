1b00a8f40e6824ba8967763fd80cd2919b74fda9
unknown <poputa.alex@gmail.com>
2024-10-08 14:46:34 +0300

    changed router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  Route, Routes, HashRouter } from 'react-router-dom';
 import TransactionHistoryPag from './pages/TransactionHistoryPag';
 import StatisticsPag from './pages/StatisticsPag';
 import ProfilePag from './pages/ProfilePag';
@@ -11,7 +11,7 @@ import App from './pages/App.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter basename='/goit-react-hw-01-components'>
+		<HashRouter>
 			<Routes>
 				<Route index path='/goit-react-hw-01-components' element={<App />} />
 				<Route path='/profile' element={<ProfilePag />} />
@@ -19,6 +19,6 @@ createRoot(document.getElementById('root')).render(
 				<Route path='/friends' element={<FriendsListPag />} />
 				<Route path='/transactions' element={<TransactionHistoryPag />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>
 );
